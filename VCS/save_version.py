9a1e6451df08f403498da2761383bea78f53fb80
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2024-06-18 03:16:59 +0300

    Функция сохранения текущей версии кода
    Реализация функции сохранения текущей версии кода

--- VCS/save_version.py
+++ VCS/save_version.py
@@ -1,6 +1,3 @@
-import database
-from tkinter import messagebox
-
 def save_version(author, code):
     database.save_version(author, code)
     messagebox.showinfo('Сохранено!', 'Версия кода успешно сохранена!')
